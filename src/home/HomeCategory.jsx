4b4b145efd8e03dbc3fa2bfe819d2d9531c5b595
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-02 22:10:12 +0330

    homeCategory sections object

---
+++ src/home/HomeCategory.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+const subTitle = "محصول خود را انتخاب کنید";
+const title = "هر چیزی که شما نیاز دارید موجود است";
+const btnText = "شروع خرید";
+
+const categoryList = [
+{
+imgUrl: 'src/assets/images/category/01.jpg',
+imgAlt: 'category rajibraj91 rajibraj',
+iconName: 'icofont-brand-windows',
+title: 'DSLR دوربین',
+},
+{
+imgUrl: 'src/assets/images/category/02.jpg',
+imgAlt: 'category rajibraj91 rajibraj',
+iconName: 'icofont-brand-windows',
+title: 'کفش',
+},
+{
+imgUrl: 'src/assets/images/category/03.jpg',
+imgAlt: 'category rajibraj91 rajibraj',
+iconName: 'icofont-brand-windows',
+title: 'لوازم عکاسی',
+},
+{
+imgUrl: 'src/assets/images/category/04.jpg',
+imgAlt: 'category rajibraj91 rajibraj',
+iconName: 'icofont-brand-windows',
+title: 'لباس فرم',
+},
+{
+imgUrl: 'src/assets/images/category/05.jpg',
+imgAlt: 'category rajibraj91 rajibraj',
+iconName: 'icofont-brand-windows',
+title: 'کیف های رنگارنگ',
+},
+{
+imgUrl: 'src/assets/images/category/06.jpg',
+imgAlt: 'category rajibraj91 rajibraj',
+iconName: 'icofont-brand-windows',
+title: 'دکور داخلی',
+},
+]
+
+
+
+
+const HomeCategory = () => {
+  return (
+    <div>HomeCategory</div>
+  )
+}
+
+export default HomeCategory
