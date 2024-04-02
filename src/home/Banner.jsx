2581c4d92a45774f98d11361e688f42eed974b22
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-02 11:43:25 +0330

    search box for prouduct

--- src/home/Banner.jsx
+++ src/home/Banner.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import productData from "../products.json";
+import { FaBell, FaGlobe, FaUser } from "react-icons/fa6";
 
+const title = (
+  <h2>
+    محصول خود را از بین <span>هزاران</span> محصول جستوجو کنید
+  </h2>
+);
+const desc = "ماز بزرگتریت بزرگ از انواع محصولات را داریم";
+
+const bannerList =[
+  {
+    icon:<FaUser/>,
+    text: "1.5 ملیون مشتری"
+  },
+  {
+    icon:<FaBell />,
+    text: "1.5 ملیون مشتری"
+  },
+  {
+    icon:<FaGlobe />,
+    text: "1.5 ملیون مشتری"
+  },
+]
 
 const Banner = () => {
-  return (
-    <div>Banner</div>
-  )
-}
+  const [serchInput,setSearchInput] = useState("")
+  const [filteredProducts,setFilteredProducts] = useState("")
 
-export default Banner
+
+  return <div className="banner-section style-4">
+    <div className="container">
+      <div className="banner-content">
+        {title}
+        <form>
+          <input type="text" name="search" id="search" placeholder="جستو جو در محصولات ..."/>
+        </form>
+      </div>
+    </div>
+  </div>;
+};
+
+export default Banner;
