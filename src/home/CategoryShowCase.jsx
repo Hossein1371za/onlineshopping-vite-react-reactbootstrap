d3636f135ad493bca07489d701dc42c49331aa4a
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-03 17:49:53 +0330

    categoryshowcase section ,object

---
+++ src/home/CategoryShowCase.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+const title = "Our Products";
+
+const ProductData = [
+{
+imgUrl: 'src/assets/images/categoryTab/01.jpg',
+cate: 'کفش',
+title: 'نایک پرمیر',
+author: 'assets/images/course/author/01.jpg',
+brand: 'Nike',
+price: '2345000',
+id: 1,
+},
+{
+imgUrl: 'src/assets/images/categoryTab/02.jpg',
+cate: 'کیف',
+title: 'کیف آرایشی',
+author: 'assets/images/course/author/02.jpg',
+brand: 'D&J کیف',
+price: '1200000',
+id: 2,
+},
+{
+imgUrl: 'src/assets/images/categoryTab/03.jpg',
+cate: 'تلفن همراه',
+title: 'iPhone 12',
+author: 'src/assets/images/categoryTab/brand/apple.png',
+brand: 'Apple',
+price: '50000000',
+id: 3,
+},
+{
+imgUrl: 'src/assets/images/categoryTab/04.jpg',
+cate: 'کیف',
+title: 'کیف پیاده روی',
+author: 'assets/images/course/author/04.jpg',
+brand: 'Gucci',
+price: '1300000',
+id: 4,
+},
+{
+imgUrl: 'src/assets/images/categoryTab/05.jpg',
+cate: 'کفش',
+title: 'کفش اسپرت ',
+author: 'assets/images/course/author/05.jpg',
+brand: 'Nike',
+price: '5000000',
+id: 5,
+},
+{
+imgUrl: 'src/assets/images/categoryTab/06.jpg',
+cate: 'زیبایی',
+title: 'COSRX Snail Mucin',
+author: 'assets/images/course/author/06.jpg',
+brand: 'Zaara',
+price: '3000000',
+id: 6,
+},
+{
+imgUrl: 'src/assets/images/categoryTab/07.jpg',
+cate: 'کیف',
+title: ' کیف شنل',
+author: 'assets/images/course/author/01.jpg',
+brand: 'chanel',
+price: '20000000',
+id: 7,
+},
+{
+imgUrl: 'src/assets/images/categoryTab/08.jpg',
+cate: ';ta',
+title: 'Sneakers',
+author: 'assets/images/course/author/02.jpg',
+brand: 'Bata',
+price: '3500000',
+id: 8,
+},
+]
+
+const CategoryShowCase = () => {
+  return (
+    <div>CategoryShowCase</div>
+  )
+}
+
+export default CategoryShowCase
