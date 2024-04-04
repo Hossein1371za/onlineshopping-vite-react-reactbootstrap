07b9b0158b559b59372ec7a97cba50a0cf1c2de9
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-04 22:14:54 +0330

    end of register section

--- src/home/Register.jsx
+++ src/home/Register.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <section className="register-section padding-tb pb-0">
+      <div className="container">
+        <div className="row g-4 row-cols-lg-2 row-cols-1 align-items-center">
+          <div className="col">
+            <div className="section-header">
+              <span className="subtitle">تو وقت صرفه جویی کن</span>
+              <h2 className="title">
+                برای صرفه جویی در وقت و
+                <b>
+                  خرید فروشگاه <span>به ما بپیوندید</span>
+                </b>
+              </h2>
+              <p>کلی زمان اضافه دیگه نیازی به عجله نیست</p>
+            </div>
+          </div>
+          <div className="col">
+            <div className="section-wrapper">
+                <h4>ثبت نام کنید</h4>
+                <form className="register-form">
+                    <input type="text" name="name" placeholder="نام..." className="reg-input"/>
+                    <input type="email" name="emaile" placeholder="ایمیل..." className="reg-input"/>
+                    <input type="number" name="number" placeholder="شماره موبایل..." className="reg-input"/>
+                    <button type="submit" className="lab-btn">
+                        ثبت نام
+                    </button>
+                </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Register
+export default Register;
