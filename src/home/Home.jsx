1c515e5577299558ebb38fe161bf423588e862d6
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-04 22:30:40 +0330

    start location section

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Banner from './Banner'
-import HomeCategory from './HomeCategory'
-import CategoryShowCase from './CategoryShowCase'
-import Register from './Register'
+import React from "react";
+import Banner from "./Banner";
+import HomeCategory from "./HomeCategory";
+import CategoryShowCase from "./CategoryShowCase";
+import Register from "./Register";
+import Location from "./Location";
 
 const Home = () => {
   return (
     <div>
-      <Banner/>
-      <HomeCategory/>
-      <CategoryShowCase/>
-      <Register/>
+      <Banner />
+      <HomeCategory />
+      <CategoryShowCase />
+      <Register />
+      <Location />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
