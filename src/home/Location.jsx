5a4c387db5f83635d693c17a4097dd5746bcc67a
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-05 18:41:54 +0330

    end of location section

--- src/home/Location.jsx
+++ src/home/Location.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 const title = "بیشتر از 60000 مشتری در جهان";
 
 const desc = "با نصب اپلیکیشن ما روی دستگاه خود از خرید لذت ببرید و در زمان صرفه جویی کنید فقط کافیست نصب کنید و شروع به خریدن کنید.";
@@ -43,7 +44,32 @@ text: 'به ما بپیوندید',
 
 const Location = () => {
   return (
-    <div>Location</div>
+    <div className='clients-section style-2 padding-tb'>
+      <div className='container'>
+        <div className='section-header text-center'>
+          <h2 className='number title'>{title}</h2>
+          <p>{desc}</p>
+        </div>
+        <div className='section-wrapper'>
+          <div className='clients'>
+            {
+              clientsList.map((item,index)=>{
+                return (
+                  <div key={index} className='client-list'>
+                    <Link to="/sing-up" className='client-content'>
+                      <span>{item.text}</span>
+                    </Link>
+                    <div className='client-thumb'>
+                      <img src={item.imgUrl} alt=''/>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </div>
   )
 } 
 
