8cf43eda20724d9f0fd9690a33fa246f63f7c21e
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-05 18:20:44 +0330

    object for location

--- src/home/Location.jsx
+++ src/home/Location.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+const title = "بیشتر از 60000 مشتری در جهان";
+
+const desc = "با نصب اپلیکیشن ما روی دستگاه خود از خرید لذت ببرید و در زمان صرفه جویی کنید فقط کافیست نصب کنید و شروع به خریدن کنید.";
+
+const clientsList = [
+{
+imgUrl: '/src/assets/images/clients/avater.jpg',
+imgAlt: 'education thumb rajibraj91 rajibraj',
+text: 'به ما بپیوندید',
+},
+{
+imgUrl: '/src/assets/images/clients/avater.jpg',
+imgAlt: 'education thumb rajibraj91 rajibraj',
+text: 'به ما بپیوندید',
+},
+{
+imgUrl: '/src/assets/images/clients/avater.jpg',
+imgAlt: 'education thumb rajibraj91 rajibraj',
+text: 'به ما بپیوندید',
+},
+{
+imgUrl: '/src/assets/images/clients/avater.jpg',
+imgAlt: 'education thumb rajibraj91 rajibraj',
+text: 'به ما بپیوندید',
+},
+{
+imgUrl: '/src/assets/images/clients/avater.jpg',
+imgAlt: 'education thumb rajibraj91 rajibraj',
+text: 'به ما بپیوندید',
+},
+{
+imgUrl: '/src/assets/images/clients/avater.jpg',
+imgAlt: 'education thumb rajibraj91 rajibraj',
+text: 'به ما بپیوندید',
+},
+{
+imgUrl: '/src/assets/images/clients/avater.jpg',
+imgAlt: 'education thumb rajibraj91 rajibraj',
+text: 'به ما بپیوندید',
+},
+]
 
 const Location = () => {
   return (
     <div>Location</div>
   )
-}
+} 
 
 export default Location
