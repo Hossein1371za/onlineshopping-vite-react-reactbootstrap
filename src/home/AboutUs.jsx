8f1b184dab5139e2c871ef5bfcc032ee86f392d4
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-05 21:59:46 +0330

    aboutus section & object

---
+++ src/home/AboutUs.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FaUserFriendsوFaUserGraduate,FaBell } from "react-icons/fa";
+
+const countList = [
+    {
+    iconName: <FaUserFriends/>,
+    count: '12600',
+    text: 'ثبت نام این ماه',
+    },
+    {
+    iconName: <FaUserGraduate/>,
+    count: '30',
+    text: 'دوره های گواهی شده',
+    },
+    {
+    iconName: <FaBell/>,
+    count: '100',
+    text: 'جوایز و کارت های هدیه',
+    },
+    ]
+
+const AboutUs = () => {
+  return (
+    <div>AboutUs</div>
+  )
+}
+
+export default AboutUs
