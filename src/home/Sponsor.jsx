3fea748aafcb82acc9046b7d44cb24df52b60953
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-07 22:37:31 +0330

    object & sponsor body

--- src/home/Sponsor.jsx
+++ src/home/Sponsor.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
 
+import {Swiper, SwiperSlide } from 'swiper/react';
+
+import "swiper/css"
+import "swiper/css/pagination"
+
+
+const sponsorList = [
+    {
+    imgUrl: "/src/assets/images/sponsor/01.png",
+    },
+    {
+    imgUrl: "/src/assets/images/sponsor/02.png",
+    },
+    {
+    imgUrl: "/src/assets/images/sponsor/03.png",
+    },
+    {
+    imgUrl: "/src/assets/images/sponsor/04.png",
+    },
+    {
+    imgUrl: "/src/assets/images/sponsor/05.png",
+    },
+    {
+    imgUrl: "/src/assets/images/sponsor/06.png",
+    },
+    ];
+
+
 const Sponsor = () => {
   return (
-    <div>Sponsor</div>
+    <div className='sponsor-section section-bg'>
+        <div className='container'>
+            <div className='section-wrapper'>
+                <div className='sponsor-slider'>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
