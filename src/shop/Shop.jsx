6edd69f3dc65e5b49b941d9d45ec3484c580cab5
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-09 23:07:40 +0330

    showing products style

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import PageHeader from '../components/PageHeader'
+import React, { useState } from "react";
+
+import { LuList, LuLayoutGrid } from "react-icons/lu";
+import PageHeader from "../components/PageHeader";
+
+const showResults = "نمایش 1 - 12 از 139 محصول";
 
 const Shop = () => {
+  const [gridList, setGridList] = useState(true);
   return (
     <div>
-        <PageHeader title="صفحه فروشگاهی ما" curPage="فروشگاه"/>
+      <PageHeader title="صفحه فروشگاهی ما" curPage="فروشگاه" />
+      <div className="shop-page padding-tb">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-12">
+              <article>
+                <div className="shop-title d-flex flex-wrap justify-content-between">
+                  <p className="number">{showResults}</p>
+                  <div
+                    className={`product-view-mode ${
+                      gridList ? "gridActive" : "listActive"
+                    }`}
+                  >
+                    <a
+                      className="grid mx-3"
+                      onClick={() => setGridList(!gridList)}
+                    >
+                      <LuLayoutGrid size={25} />
+                    </a>
+                    <a className="list" onClick={() => setGridList(!gridList)}>
+                      <LuList size={25} />
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </div>
+            <div className="col-lg-4 col-12">left side</div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
