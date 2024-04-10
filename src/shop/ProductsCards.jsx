5b4dc606898f20a4cd9e16d4b3c5bdea45ccd39b
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-10 19:54:01 +0330

    shoing product col & row

--- src/shop/ProductsCards.jsx
+++ src/shop/ProductsCards.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { FaEye, FaHeart, FaShoppingCart } from "react-icons/fa";
+import { FaEye, FaHeart, FaShoppingCart,FaStar } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const ProductsCards = ({ gridList, products }) => {
@@ -13,14 +13,14 @@ const ProductsCards = ({ gridList, products }) => {
       {products.map((product) => {
         return (
           <div className="col-lg-4 col-md-6 col-12" key={products.id}>
-            <div className="product-item">
+            <div className={`${gridList ? "product-item" : "product-list-item"}`}>
               <div className="product-thumb">
                 <div className="pro-thumb">
                   <img src={product.img} alt="" />
                 </div>
                 <div className="product-action-link">
                   <Link to={`/shop/${product.id}`}>
-                    <FaEye/>
+                    <FaEye />
                   </Link>
                   <a href="#">
                     <FaHeart />
@@ -29,6 +29,19 @@ const ProductsCards = ({ gridList, products }) => {
                     <FaShoppingCart />
                   </Link>
                 </div>
+              </div>
+              <div className="product-content">
+                <h5>
+                  <Link to={`/shop/${product.id}`}>{product.name}</Link>
+                </h5>
+                <p className="productRating">
+                    <FaStar className="yellow-color"/>
+                    <FaStar className="yellow-color"/>
+                    <FaStar className="yellow-color"/>
+                    <FaStar className="yellow-color"/>
+                    <FaStar className="yellow-color"/>
+                </p>
+                <h6>قیمت : {product.price}</h6>
               </div>
             </div>
           </div>
