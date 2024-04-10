fe5789d0863239e2e1725a1b1b64b085c3682c44
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-10 19:22:20 +0330

    products link hover

--- src/shop/ProductsCards.jsx
+++ src/shop/ProductsCards.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { FaEye, FaHeart, FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const ProductsCards = ({ gridList, products }) => {
   return (
     <div
@@ -11,11 +14,22 @@ const ProductsCards = ({ gridList, products }) => {
         return (
           <div className="col-lg-4 col-md-6 col-12" key={products.id}>
             <div className="product-item">
-                <div className="product-thumb">
-                    <div className="pro-thumb">
-                        <img src={product.img} alt=""/>
-                    </div>
+              <div className="product-thumb">
+                <div className="pro-thumb">
+                  <img src={product.img} alt="" />
                 </div>
+                <div className="product-action-link">
+                  <Link to={`/shop/${product.id}`}>
+                    <FaEye/>
+                  </Link>
+                  <a href="#">
+                    <FaHeart />
+                  </a>
+                  <Link to="/cart-page">
+                    <FaShoppingCart />
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         );
