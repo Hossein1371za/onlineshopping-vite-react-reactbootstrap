8c43cdc0f22dd2dec57d20e737f9d0e66de6e4ac
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-16 23:51:06 +0330

    single product

--- src/shop/SingleProduct.jsx
+++ src/shop/SingleProduct.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios';
+import PageHeader from '../components/PageHeader';
 
 const SingleProduct = () => {
+    const [product,setProduct] = useState([])
+    const {id} = useParams()
+    useEffect(()=>{
+        fetch("/src/product.json").then(res => res.json()).then(data => setProduct(data))
+        // with axios
+        // axios.get("/src/product.json")
+        // .then(response => {
+        //     setProduct(response.data);
+        // })
+        // .catch(error => {
+        //     console.error("Error fetching data:", error);
+        // });
+    },[])
+    const result = product.filter((p) => p.id === id)
   return (
-    <div>SingleProduct</div>
+    <div>
+        <PageHeader title={"محصول مورد نظر شما"} curPage={" فروشگاه/ محصول "}/>
+    </div>
   )
 }
 
-export default SingleProduct
+export default SingleProduct 
