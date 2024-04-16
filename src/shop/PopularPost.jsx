6b5c3a563c99b19498d682752e9abce2ed1a933d
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-16 21:01:52 +0330

    popularposts object

--- src/shop/PopularPost.jsx
+++ src/shop/PopularPost.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+
+const title = "پست های محبوب"
+
+
+const postList = [
+  {
+    id: 1,
+    imgUrl: "/src/assets/images/blog/10.jpg",
+    imgAlt: "rajibraj91",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    date: "Jun 05,2022",
+  },
+  {
+    id: 2,
+    imgUrl: "/src/assets/images/blog/11.jpg",
+    imgAlt: "rajibraj91",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    date: "Jun 05,2022",
+  },
+  {
+    id: 3,
+    imgUrl: "/src/assets/images/blog/12.jpg",
+    imgAlt: "rajibraj91",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    date: "Jun 05,2022",
+  },
+  {
+    id: 4,
+    imgUrl: "/src/assets/images/blog/09.jpg",
+    imgAlt: "rajibraj91",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    date: "Jun 05,2022",
+  },
+];
 
 const PopularPost = () => {
-  return (
-    <div>PopularPost</div>
-  )
-}
+  return <div>x</div>;
+};
 
-export default PopularPost
+export default PopularPost;
