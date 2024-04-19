817d1985d2e1f7a4c4c69ab515b363abceec3fe5
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-19 21:27:38 +0330

    review description

--- src/shop/Reviwe.jsx
+++ src/shop/Reviwe.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 const reviwtitle = "اضافه کردن بازدید";
 
 let ReviewList = [
@@ -33,7 +34,52 @@ let ReviewList = [
 ];
 
 const Reviwe = () => {
-  return <div>Reviwe</div>;
+  const [reviewShow, setReviewShow] = useState(true);
+
+  return (
+    <>
+      <ul
+        className={`review-nav lab-ul ${
+          reviewShow ? "RevActive" : "DescActive"
+        }`}
+      >
+        <li className="desc" onClick={() => setReviewShow(!reviewShow)}>
+          توضیحات
+        </li>
+        <li className="rev" onClick={() => setReviewShow(!reviewShow)}>
+          بازدید
+        </li>
+      </ul>
+      <div className={`review-content ${reviewShow ? "review-content-show" : "description-show"}`}>
+        <div className="review-showing">
+            <ul className="content lab-ul">
+                {
+                    ReviewList.map((review,i)=>{
+                        return(
+                            <li key={i}>
+                                <div className="post-thumb">
+                                    <img src={review.imgUrl} alt={review.imgAlt} />
+                                </div>
+                                <div className="post-content">
+                                    <div className="entry-meta">
+                                        <div className="posted-on">
+                                            <a href="#">{review.name}</a>
+                                            <p>{review.date}</p>
+                                        </div>
+                                    </div>
+                                    <div className="entry-content">
+                                        <p>{review.desc}</p>
+                                    </div>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Reviwe;
