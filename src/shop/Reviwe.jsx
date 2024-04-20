1f3b7e369ba290ebf0319b667b28f8943831b5cf
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-20 23:23:13 +0330

    description review

--- src/shop/Reviwe.jsx
+++ src/shop/Reviwe.jsx
@@ -51,65 +51,131 @@ const Reviwe = () => {
           بازدید
         </li>
       </ul>
-      <div className={`review-content ${reviewShow ? "review-content-show" : "description-show"}`}>
+      <div
+        className={`review-content ${
+          reviewShow ? "review-content-show" : "description-show"
+        }`}
+      >
         <div className="review-showing">
-            <ul className="content lab-ul">
-                {
-                    ReviewList.map((review,i)=>{
-                        return(
-                            <li key={i}>
-                                <div className="post-thumb">
-                                    <img src={review.imgUrl} alt={review.imgAlt} />
-                                </div>
-                                <div className="post-content">
-                                    <div className="entry-meta">
-                                        <div className="posted-on">
-                                            <a href="#">{review.name}</a>
-                                            <p>{review.date}</p>
-                                        </div>
-                                    </div>
-                                    <div className="entry-content">
-                                        <p>{review.desc}</p>
-                                    </div>
-                                </div>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-            <div className="client-review">
-                <div className="review-form">
-                    <div className="review-title">
-                        <h5>{reviwtitle}</h5>
+          <ul className="content lab-ul">
+            {ReviewList.map((review, i) => {
+              return (
+                <li key={i}>
+                  <div className="post-thumb">
+                    <img src={review.imgUrl} alt={review.imgAlt} />
+                  </div>
+                  <div className="post-content">
+                    <div className="entry-meta">
+                      <div className="posted-on">
+                        <a href="#">{review.name}</a>
+                        <p>{review.date}</p>
+                      </div>
                     </div>
-                    <form className="row">
-                        <div className="col-md-4 col-12">
-                            <input type="text" name="name" id="name"  placeholder="نام و نام خانوادگی..."/>
-                        </div>
-                        <div className="col-md-4 col-12">
-                            <input type="email" name="email" id="email"  placeholder="ایمیل..."/>
-                        </div>
-                        <div className="col-md-4 col-12">
-                            <div className="rating">
-                                <span className="ms-2">امتیاز شما</span>
-                                <FaStar className="text-warning"/>
-                                <FaStar className="text-warning"/>
-                                <FaStar className="text-warning"/>
-                                <FaStar className="text-warning"/>
-                                <FaStar className="text-warning"/>
-                            </div>
-                        </div>
-                        <div className="col-md-12 col-12">
-                            <textarea name="massage" id="massage" rows={8} placeholder="نظر شما ..."></textarea>
-                        </div>
-                        <div className="col-12">
-                            <button type="submit" className="default-button">
-                                <span>ارسال نظر</span>
-                            </button>
-                        </div>
-                    </form>
+                    <div className="entry-content">
+                      <p>{review.desc}</p>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="client-review">
+            <div className="review-form">
+              <div className="review-title">
+                <h5>{reviwtitle}</h5>
+              </div>
+              <form className="row">
+                <div className="col-md-4 col-12">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="نام و نام خانوادگی..."
+                  />
                 </div>
+                <div className="col-md-4 col-12">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="ایمیل..."
+                  />
+                </div>
+                <div className="col-md-4 col-12">
+                  <div className="rating">
+                    <span className="ms-2">امتیاز شما</span>
+                    <FaStar className="text-warning" />
+                    <FaStar className="text-warning" />
+                    <FaStar className="text-warning" />
+                    <FaStar className="text-warning" />
+                    <FaStar className="text-warning" />
+                  </div>
+                </div>
+                <div className="col-md-12 col-12">
+                  <textarea
+                    name="massage"
+                    id="massage"
+                    rows={8}
+                    placeholder="نظر شما ..."
+                  ></textarea>
+                </div>
+                <div className="col-12">
+                  <button type="submit" className="default-button">
+                    <span>ارسال نظر</span>
+                  </button>
+                </div>
+              </form>
             </div>
+          </div>
+        </div>
+        <div className="description">
+          <p>
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+            استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
+            کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد کتابهای زیادی
+            در شصت و سه درصد گذشته حال و آینده شناخت فراوان جامعه و متخصصان را
+            می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+            الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد
+          </p>
+          <div className="post-item">
+            <div className="post-thumb">
+              <img src="/src/assets/images/shop/01.jpg" alt="" />
+            </div>
+            <div className="post-content">
+              <ul className="lab-ul">
+                <li>
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                  استفاده از طراحان گرافیک است
+                </li>
+                <li>
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                </li>
+                <li>
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                </li>
+                <li>
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                </li>
+                <li>
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                </li>
+                <li>
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با                </li>
+                <li>
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با                </li>
+              </ul>
+              <p>
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+            استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
+            کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد کتابهای زیادی
+            در شصت و سه درصد گذشته حال و آینده شناخت فراوان جامعه و متخصصان را
+            می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+            الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد
+          </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
