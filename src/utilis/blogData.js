7a2b9f2fd4d76d2a2d9653c7e5da6e430d85bcab
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-25 17:54:26 +0330

    blog data

---
+++ src/utilis/blogData.js
@@ -0,0 +1,176 @@
+import { FaRegUser, FaCalendarPlus } from "react-icons/fa";
+const blogList = [
+  {
+    id: 1,
+    imgUrl: "/src/assets/images/blog/01.jpg",
+    imgAlt: "Blog Thumb",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله ",
+    commentCount: "3",
+    btnText: "Read More",
+    metaList: [
+      {
+        iconName: <FaRegUser />,
+        text: "حسین",
+      },
+      {
+        iconName: <FaCalendarPlus />,
+        text: "Jun 05,2022",
+      },
+    ],
+  },
+  {
+    id: 2,
+    imgUrl: "/src/assets/images/blog/02.jpg",
+    imgAlt: "Blog Thumb",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله ",
+    commentCount: "3",
+    btnText: "Read More",
+    metaList: [
+      {
+        iconName: <FaRegUser />,
+        text: "حسین",
+      },
+      {
+        iconName: <FaCalendarPlus />,
+        text: "Jun 05,2022",
+      },
+    ],
+  },
+  {
+    id: 3,
+    imgUrl: "/src/assets/images/blog/03.jpg",
+    imgAlt: "Blog Thumb",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله ",
+    commentCount: "3",
+    btnText: "Read More",
+    metaList: [
+      {
+        iconName: <FaRegUser />,
+        text: "حسین",
+      },
+      {
+        iconName: <FaCalendarPlus />,
+        text: "Jun 05,2022",
+      },
+    ],
+  },
+  {
+    id: 4,
+    imgUrl: "/src/assets/images/blog/04.jpg",
+    imgAlt: "Blog Thumb",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله ",
+    commentCount: "3",
+    btnText: "Read More",
+    metaList: [
+      {
+        iconName: <FaRegUser />,
+        text: "حسین",
+      },
+      {
+        iconName: <FaCalendarPlus />,
+        text: "Jun 05,2022",
+      },
+    ],
+  },
+  {
+    id: 5,
+    imgUrl: "/src/assets/images/blog/05.jpg",
+    imgAlt: "Blog Thumb",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله ",
+    commentCount: "3",
+    btnText: "Read More",
+    metaList: [
+      {
+        iconName: <FaRegUser />,
+        text: "حسین",
+      },
+      {
+        iconName: <FaCalendarPlus />,
+        text: "Jun 05,2022",
+      },
+    ],
+  },
+  {
+    id: 6,
+    imgUrl: "/src/assets/images/blog/06.jpg",
+    imgAlt: "Blog Thumb",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله ",
+    commentCount: "3",
+    btnText: "Read More",
+    metaList: [
+      {
+        iconName: <FaRegUser />,
+        text: "حسین",
+      },
+      {
+        iconName: <FaCalendarPlus />,
+        text: "Jun 05,2022",
+      },
+    ],
+  },
+  {
+    id: 7,
+    imgUrl: "/src/assets/images/blog/07.jpg",
+    imgAlt: "Blog Thumb",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله ",
+    commentCount: "3",
+    btnText: "Read More",
+    metaList: [
+      {
+        iconName: <FaRegUser />,
+        text: "حسین",
+      },
+      {
+        iconName: <FaCalendarPlus />,
+        text: "Jun 05,2022",
+      },
+    ],
+  },
+  {
+    id: 8,
+    imgUrl: "/src/assets/images/blog/08.jpg",
+    imgAlt: "Blog Thumb",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله ",
+    commentCount: "3",
+    btnText: "Read More",
+    metaList: [
+      {
+        iconName: <FaRegUser />,
+        text: "حسین",
+      },
+      {
+        iconName: <FaCalendarPlus />,
+        text: "Jun 05,2022",
+      },
+    ],
+  },
+  {
+    id: 9,
+    imgUrl: "/src/assets/images/blog/09.jpg",
+    imgAlt: "Blog Thumb",
+    title: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم",
+    desc: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله ",
+    commentCount: "3",
+    btnText: "Read More",
+    metaList: [
+      {
+        iconName: <FaRegUser />,
+        text: "حسین",
+      },
+      {
+        iconName: <FaCalendarPlus />,
+        text: "Jun 05,2022",
+      },
+    ],
+  },
+];
+
+export default blogList;
