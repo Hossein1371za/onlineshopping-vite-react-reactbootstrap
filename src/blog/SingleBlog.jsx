48779f0f8aa8134cabea352a93634a2f2e88cf38
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-27 22:43:41 +0330

    single blog

--- src/blog/SingleBlog.jsx
+++ src/blog/SingleBlog.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import blogList from '../utilis/blogData'
+import { useParams } from 'react-router-dom'
+import PageHeader from '../components/PageHeader'
 
 const SingleBlog = () => {
+  const [blogs,setBlogs] = useState(blogList)
+  const {id} = useParams()
+  const blog = blogs.filter((item)=> item.id === Number(id))
+  
   return (
-    <div>SingleBlog</div>
+    <div>
+      <PageHeader title={"اطلاعات بلاگ"} curPage={"بلاگ / اطلاعات بلاگ انتخابی"}/>
+    </div>
   )
 }
 
