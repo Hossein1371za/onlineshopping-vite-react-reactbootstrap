859f08a3316415a7645ae135d4e3031bb82e9fb3
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-29 22:25:02 +0330

    about section

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PageHeader from "../components/PageHeader";
 
 const subTitle = "درباره شرکت ما";
 const title = "خدمات و تجربیات ما";
@@ -30,7 +31,57 @@ const aboutList = [
 ];
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <PageHeader title={subTitle} curPage={"درباره ما"} />
+      <div className="about-section style-3 padding-tb section-bg">
+        <div className="container">
+          <div className="row justify-content-center row-cols-xl-2 row-cols-1 align-items-center">
+          <div className="col">
+                <div className="about-right">
+                    <div className="section-header">
+                        <span className="subtitle">{subTitle}</span>
+                        <h2 className="title">{title}</h2>
+                        <p>{desc}</p>
+                    </div>
+                    <div className="section-wrapper">
+                        <ul className="lab-ul">
+                            {
+                                aboutList.map((item,i)=>(
+                                    <li key={i}>
+                                        <div className="sr-left">
+                                            <img src={item.imgUrl} alt=""/>
+                                        </div>
+                                        <div className="sr-right">
+                                            <h5>{item.title}</h5>
+                                            <p>{item.desc}</p>
+                                        </div>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+              <div className="about-left">
+                <div className="about-thumb">
+                  <img src="/src/assets/images/about/01.jpg" alt="" />
+                </div>
+                <div className="abs-thumb">
+                  <img src="/src/assets/images/about/02.jpg" alt="" />
+                </div>
+                <div className="about-left-content">
+                  <h3 className="number">{year}</h3>
+                  <p>{expareance}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
