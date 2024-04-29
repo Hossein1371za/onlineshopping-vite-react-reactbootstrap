abe5e9fb4125be2fcfbb9dc80ebb30902fe01c7e
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-04-29 19:38:51 +0330

    single blog

--- src/blog/SingleBlog.jsx
+++ src/blog/SingleBlog.jsx
@@ -2,7 +2,22 @@ import React, { useState } from "react";
 import blogList from "../utilis/blogData";
 import { useParams } from "react-router-dom";
 import PageHeader from "../components/PageHeader";
-import { FaPlay } from "react-icons/fa";
+import {
+  FaPlay,
+  FaInstagram,
+  FaTelegram,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitterSquare,
+} from "react-icons/fa";
+
+const socialList = [
+  { iconName: <FaFacebook />, siteLink: "#", className: "facebook" },
+  { iconName: <FaTwitterSquare />, siteLink: "#", className: "twitter" },
+  { iconName: <FaLinkedin />, siteLink: "#", className: "linkedin" },
+  { iconName: <FaInstagram />, siteLink: "#", className: "instagram" },
+  { iconName: <FaTelegram />, siteLink: "#", className: "pinterest" },
+];
 
 const SingleBlog = () => {
   const [blogs, setBlogs] = useState(blogList);
@@ -118,10 +133,55 @@ const SingleBlog = () => {
                                     گیرد.
                                   </p>
                                   <div className="video-thumb">
-                                     <img src="/src/assets/images/blog/single/02.jpg" alt=""/>
-                                     <a href="https://youtu.be/_W3R2VwRyF4?si=dQrQfEMwVnu01v6x" className="video-button popup">
-                                      <i><FaPlay/></i>
-                                     </a>
+                                    <img
+                                      src="/src/assets/images/blog/single/02.jpg"
+                                      alt=""
+                                    />
+                                    <a
+                                      href="https://youtu.be/_W3R2VwRyF4?si=dQrQfEMwVnu01v6x"
+                                      className="video-button popup"
+                                    >
+                                      <i>
+                                        <FaPlay />
+                                      </i>
+                                    </a>
+                                  </div>
+                                  <p>
+                                    لورم ایپسوم متن ساختگی با تولید سادگی
+                                    نامفهوم از صنعت چاپ و با استفاده از طراحان
+                                    گرافیک است. چاپگرها و متون بلکه روزنامه و
+                                    مجله در ستون و سطرآنچنان که لازم است و برای
+                                    شرایط فعلی تکنولوژی مورد نیاز و کاربردهای
+                                    متنوع با هدف بهبود ابزارهای کاربردی می باشد.
+                                    کتابهای زیادی در شصت و سه درصد گذشته، حال و
+                                    آینده شناخت فراوان جامعه و متخصصان را می
+                                    طلبد تا با نرم افزارها شناخت بیشتری را برای
+                                    طراحان رایانه ای علی الخصوص طراحان خلاقی و
+                                    فرهنگ پیشرو در زبان فارسی ایجاد کرد.
+                                  </p>
+                                  <div className="tags-section">
+                                    <ul className="tags lab-ul">
+                                      <li>
+                                        <a href="#">آژانس</a>
+                                      </li>
+                                      <li>
+                                        <a href="#">بیزینس</a>
+                                      </li>
+                                      <li>
+                                        <a href="#">شخصی</a>
+                                      </li>
+                                    </ul>
+                                    <ul className="lab-ul social-icons">
+                                      {
+                                        socialList.map((social,i)=>(
+                                          <li key={i}>
+                                            <a href="#" className={social.className}>
+                                              <i>{social.iconName}</i>
+                                            </a>
+                                          </li>
+                                        ))
+                                      }
+                                    </ul>
                                   </div>
                                 </div>
                               </div>
