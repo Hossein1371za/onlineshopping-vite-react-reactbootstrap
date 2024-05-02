e06294f69321abd372b32c01eb22d550626e52c2
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-05-02 17:54:26 +0330

    env local

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,23 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAy1GGAZKVxf7Hd4ijV0d_8g0DVPL30m9w",
-  authDomain: "shopping-cart-reactjs-c4432.firebaseapp.com",
-  projectId: "shopping-cart-reactjs-c4432",
-  storageBucket: "shopping-cart-reactjs-c4432.appspot.com",
-  messagingSenderId: "546509524007",
-  appId: "1:546509524007:web:a647c0bf8e0a884f3e9202",
-  measurementId: "G-P4D02FYNBL",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId:  import.meta.env.VITE_PROJECTID,
+  storageBucket:  import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:  import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:  import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export default app;
